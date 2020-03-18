44f704b481f4d427882796264b9b45e21f25b6eb
Roy van Santen <royvsanten@gmail.com>
2020-03-18 19:18:37 +0100

    Small refactor

--- src/main/scala/chapter17/PrintUrlsOnPage.scala
+++ src/main/scala/chapter17/PrintUrlsOnPage.scala
@@ -6,9 +6,7 @@ import scala.concurrent.{Await, Future}
 // NOTE: Make sure to type a URL, then add a space, then press enter. Otherwise the URL will
 // be opened in a browser
 object PrintUrlsOnPage extends App {
-  import scala.concurrent.ExecutionContext.Implicits.global
-
   val f = ExecuteSequentially.visitUrlsOnPage()
-  Await.ready(f, Duration.fromNanos(10000000000L))
+  Await.ready(f, Duration.Inf)
   f.value.get.foreach(l => println(l))
 }
